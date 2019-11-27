6e1f710775df371804e6b34bddcc2bccb713da40
wanghu <1158245466@qq.com>
2019-11-27 09:29:46 +0800

    commit

--- api_learning/02——useModule.js
+++ api_learning/02——useModule.js
@@ -1,4 +1,45 @@
 const os =require('os')
-const mem =os.freemem()/os.totalmem()
-console.log(mem)
-console.log(`${mem.toFixed(2)}%`)
+// const mem =os.freemem()/os.totalmem()
+// console.log(mem)
+// console.log(`${mem.toFixed(2)}%`)
+
+// const download =require('download-git-repo')
+
+
+let repo='github:su37josephxia/vue-template'
+let dest='test-dowmload'
+
+
+
+
+// 回调式
+
+// download(repo,dest,(err)=>{
+//     if(err){
+//         console.error('error!')
+//         progress.fail()
+//     }else{
+//         console.log('OK')
+//         progress.succeed()
+//     }
+// })
+
+// async await
+
+
+
+clone(repo,dest)
+async function clone(repo,dest){
+    const {promisify} =require('util') //先处理成promise对象
+    const download =promisify(require('download-git-repo')) 
+    const ora =require('ora')
+    const progress=ora(`下载中${repo}`)
+    progress.start()
+    progress.color='yellow'
+    try{
+        await download(repo,dest)
+    } catch{
+        progress.fail()
+    }
+    progress.succeed()
+}
